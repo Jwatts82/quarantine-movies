fdcf4242f96da33ad1da1494aada36037d568c92
jwatts82 <jessperr182@hotmail.com>
2021-01-05 21:28:50 -0800

    create a categories and movies index

--- movies-frontend/index.js
+++ movies-frontend/index.js
@@ -4,11 +4,38 @@ window.addEventListener("DOMContentLoaded", () => {
     getCategories()
 })
 
+//categories index
 function getCategories() {
     let main = document.getElementById('main')
+    main.innerHTML = "" //blank the page and put categories on
     fetch(BASE_URL + '/categories')
     .then(res => res.json())
-    .then(categories => {
-        console.log(categories)
+    .then(categories => categories.map(category => {
+        
+        main.innerHTML += `
+        <li>
+            <a href="#" data-id="${category.id}">${category.name}</a>
+        </li>
+        ` 
     })
-}
+    attachClicksToLinks()
+)}
+
+//movies index view
+function getMovies() {
+    let main = document.getElementById('main')
+    main.innerHTML = "" //blank the page and put movies on
+    fetch(BASE_URL + '/movies')
+    .then(res => res.json())
+    .then(movies => movies.map(movie => {
+
+        main.innerHTML += `
+        <li>
+            <a href="#" data-id="${movie.id}">${movie.title}</a>
+            - ${movie.watched ? "Watched" : "Not Watched"}
+        </li>
+        ` 
+    })
+    attachClicksToLinks()
+)}
+
